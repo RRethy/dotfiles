e5c06a65d3652d015dae1d44ea01cc3447d38fc9
Adam P. Regasz-Rethy <rethy.spud@gmail.com>
2018-07-12 21:33:51 -0700

    Install script

--- install_rrethy.sh
+++ install_rrethy.sh
@@ -1,1 +1,29 @@
+#!/bin/bash
+
+# Setup ideavim {{{
+echo "[Ideavim] Status: Started"
+
 echo "source ~/.config/ideavimrc.vim" >> ~/.ideavimrc
+
+echo "[Ideavim] Status: Finished!"
+#}}}
+
+# Setup gitconfig {{{
+echo "[Gitconfig] Status: Started"
+
+echo "[include]" >> ~/.gitconfig
+echo "  path = ~/.config/git/.gitconfig" >> ~/.gitconfig
+
+echo "[Gitconfig] Status: Finished!"
+#}}}
+
+# Setup tmux {{{
+echo "[tmux] Status: Started"
+
+source-file "~/.config/tmux/tmux.conf"
+
+echo "[tmux] Status: Finished!"
+#}}}
+
+
+# vim: foldmethod=marker
