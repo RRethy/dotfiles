195d7c9bb7be0198e522d05fd528c9fb48121fba
Adam P. Regasz-Rethy <adam.regaszrethy@shopify.com>
2023-01-01 21:13:44 -0500

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,37 +1,53 @@
 #! /bin/sh
 
-# mkdir -p ~/.config
+wget https://github.com/bufbuild/buf/releases/download/v1.11.0/buf-Linux-x86_64
+mv buf-Linux-x86_64 buf
+chmod +x buf
+mkdir -p "$HOME/bin"
+mv buf "$HOME/bin"
+
+mkdir -p "$HOME/.config"
 
 # git setup
-# mkdir -p ~/.config/git/bin
-# ln -sf ~/dotfiles/git/config ~/.config/git/config
-# ln -sf ~/dotfiles/git/gitignore_global ~/.config/git/gitignore_global
-# ln -sf ~/dotfiles/git/bin/git-jump ~/.config/git/bin/git-jump
+mkdir -p "$HOME/.config/git/bin"
+ln -sf "$HOME/dotfiles/git/config"           "$HOME/.config/git/config"
+ln -sf "$HOME/dotfiles/git/gitignore_global" "$HOME/.config/git/gitignore_global"
+ln -sf "$HOME/dotfiles/git/bin/git-jump"     "$HOME/.config/git/bin/git-jump"
 
 # neovim setup
-# ln -sf ~/dotfiles/nvim ~/.config/nvim
-
-# tagrity setup
-# ln -sf ~/dotfiles/tagrity ~/.config/tagrity
+ln -sf "$HOME/dotfiles/nvim" "$HOME/.config/nvim"
 
 # zshrc setup
-# mkdir -p ~/.config/zsh
-# ln -sf ~/dotfiles/zsh/.zshenv ~/.zshenv
-# ln -sf ~/dotfiles/zsh/.zshrc ~/.config/zsh/.zshrc
-# ln -sf ~/dotfiles/zsh/gitprompt.sh ~/.config/zsh/gitprompt.sh
+mkdir -p "$HOME/.config/zsh"
+ln -sf "$HOME/dotfiles/zsh/.zshenv"      "$HOME/.zshenv"
+ln -sf "$HOME/dotfiles/zsh/.zshrc"       "$HOME/.config/zsh/.zshrc"
+ln -sf "$HOME/dotfiles/zsh/gitprompt.sh" "$HOME/.config/zsh/gitprompt.sh"
 
 # specify the colours I use
-# ln -sf ~/dotfiles/.base16_theme ~/.config/.base16_theme
+ln -sf "$HOME/dotfiles/.base16_theme" "$HOME/.config/.base16_theme"
 
 # https://sw.kovidgoyal.net/kitty/faq/#i-get-errors-about-the-terminal-being-unknown-or-opening-the-terminal-failing-when-sshing-into-a-different-computer
 tic -x -o \~/.terminfo ~/dotfiles/kitty/infocmp.txt
 
-# install rust
-# curl https://sh.rustup.rs -sSf | sh -s -- -y
+sudo apt-get install -y fzy
+sudo apt-get install -y fzf
+sudo apt-get install -y ripgrep
+# sudo apt-get install fd-find
 
-# install some of my own command line tools
-# cargo install --git https://github.com/RRethy/rwc
-# cargo install --git https://github.com/RRethy/tcount.git
+# install Neovim
+sudo apt purge neovim
+sudo add-apt-repository -y ppa:neovim-ppa/unstable
+sudo apt update
+sudo apt install -y neovim
+
+nvim -c "PackUpdate"
+cd "$XDG_DATA_HOME/nvim/site/pack/backpack/opt/telescope-fzy-native.nvim/"
+git submodule update
+git submodule init
+cd "$XDG_DATA_HOME/nvim/site/pack/backpack/opt/telescope-fzy-native.nvim/"
+make
+sudo rm -rf /usr/local/lib/nvim/parser
+sudo rm -rf /usr/local/share/nvim/runtime/queries
 
 # OSX specific setup
 # if [[ $OSTYPE == "darwin"* ]]; then
